42276ca24d583b519f38743ff7ff1cebb45ee6f5
Bo Zhang <bo@gradle.com>
2018-03-05 09:10:07 +0800

    Simplify sample to highlight the issue

--- src/main/kotlin/com/mkobit/gradle/test/kotlin/testkit/runner/DefaultKBuildTask.kt
+++
@@ -1,5 +0,0 @@
-package com.mkobit.gradle.test.kotlin.testkit.runner
-
-import org.gradle.testkit.runner.BuildTask
-
-internal class DefaultKBuildTask(private val delegate: BuildTask) : KBuildTask, BuildTask by delegate
